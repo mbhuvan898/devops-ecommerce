449fc417a66741c01a494652519de9ed33dfdb9f
Bhuvan M <mbhuvan898@gmail.com>
2026-01-03 23:10:36 +0530

    s3newwith new prduct

--- backend/utils/searchFeatures.js
+++ backend/utils/searchFeatures.js
@@ -1,59 +1,96 @@
-// backend/utils/searchFeatures.js
-
 class SearchFeatures {
     constructor(query, queryString) {
         this.query = query;
         this.queryString = queryString;
     }
 
-    // Search by product name (keyword search)
+    // ===============================
+    // Keyword search (Product Name)
+    // ===============================
     search() {
-        const keyword = this.queryString.keyword ? {
-            name: {
-                $regex: this.queryString.keyword,
-                $options: "i", // case insensitive
-            }
-        } : {};
-
-        this.query = this.query.find({ ...keyword });
-        return this;
-    }
-
-    // Filter by category, price, ratings, etc.
-    filter() {
-        const queryCopy = { ...this.queryString };
-
-        // Remove fields that should not be part of the filter query
-        const removeFields = ["keyword", "page", "limit", "subcategory"];
-        removeFields.forEach(key => delete queryCopy[key]);
-
-        // Convert price and ratings operators (gt, gte, lt, lte) to MongoDB operators
-        // Example: price[gte]=1000 becomes { price: { $gte: 1000 } }
-        let queryString = JSON.stringify(queryCopy);
-        queryString = queryString.replace(/\b(gt|gte|lt|lte)\b/g, key => `$${key}`);
-
-        // Apply the filter to the query
-        this.query = this.query.find(JSON.parse(queryString));
-        return this;
-    }
-
-    // Filter by subcategory
-    subcategoryFilter() {
-        if (this.queryString.subcategory) {
-            // Filter products where subcategory matches
-            this.query = this.query.find({ 
-                subcategory: this.queryString.subcategory 
+        if (this.queryString.keyword) {
+            this.query = this.query.find({
+                name: {
+                    $regex: this.queryString.keyword,
+                    $options: "i", // case-insensitive
+                },
             });
         }
         return this;
     }
 
+    // ===============================
+    // Generic filters (price, ratings)
+    // ===============================
+    filter() {
+        const queryCopy = { ...this.queryString };
+
+        // Remove non-filter fields
+        const removeFields = [
+            "keyword",
+            "page",
+            "limit",
+            "category",
+            "subcategory",
+            "productType",
+        ];
+
+        removeFields.forEach((key) => delete queryCopy[key]);
+
+        // Convert operators to MongoDB syntax
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(
+            /\b(gt|gte|lt|lte)\b/g,
+            (key) => `$${key}`
+        );
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    // ===============================
+    // Category / Subcategory / Type
+    // (CASE-INSENSITIVE & SAFE)
+    // ===============================
+    subcategoryFilter() {
+        const filter = {};
+
+        if (this.queryString.category) {
+            filter.category = {
+                $regex: `^${this.queryString.category}$`,
+                $options: "i",
+            };
+        }
+
+        if (this.queryString.subcategory) {
+            filter.subcategory = {
+                $regex: `^${this.queryString.subcategory}$`,
+                $options: "i",
+            };
+        }
+
+        if (this.queryString.productType) {
+            filter.productType = {
+                $regex: `^${this.queryString.productType}$`,
+                $options: "i",
+            };
+        }
+
+        if (Object.keys(filter).length > 0) {
+            this.query = this.query.find(filter);
+        }
+
+        return this;
+    }
+
+    // ===============================
     // Pagination
+    // ===============================
     pagination(resultPerPage) {
         const currentPage = Number(this.queryString.page) || 1;
-        const skipProducts = resultPerPage * (currentPage - 1);
+        const skip = resultPerPage * (currentPage - 1);
 
-        this.query = this.query.limit(resultPerPage).skip(skipProducts);
+        this.query = this.query.limit(resultPerPage).skip(skip);
         return this;
     }
 }
